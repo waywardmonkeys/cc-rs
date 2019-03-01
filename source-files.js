f96076202d233359cd718d894a9bf112206d9ca8
Deploy from CI <>
2019-03-01 20:08:05 +0000

    Deploy 9e57a2b0e922a96be5d28b1adf6e5fc41d5d762c to gh-pages

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["cc"] = {"name":"","dirs":[],"files":["lib.rs","windows_registry.rs"]};
+sourcesIndex["gcc_shim"] = {"name":"","dirs":[],"files":["gcc-shim.rs"]};
+createSourceSidebar();
